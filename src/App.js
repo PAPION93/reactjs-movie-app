47fdb9bea6aae001f330ebb032bf919abba36868
papion <sjun1319@gmail.com>
2020-04-12 17:25:07 +0900

    Complete get movie list

--- src/App.js
+++ src/App.js
@@ -1,44 +1,50 @@
 import React from "react";
+// import PropTypes from "prop-types"
+import axios from "axios";
+import Movie from "./Movie";
 
+class App extends React.Component {
+	state = {
+		isLoading: true
+	};
 
-const foodILike = [
-  {
-    id:1,
-    name:"Kimchi",
-    image:"http://minamkimchi.com/web/product/big/201606/18_shop1_290903.jpg"
-  },
-  {
-    id:2,
-    name:"Ramen",
-    image:"https://pds.joins.com/news/component/newsis/201809/21/NISI20180921_0000205352_web.jpg"
-  }
-];
+	getMovies = async () => {
+		const {
+			data: {
+				data: { movies }
+			}
+		} = await axios.get(
+			"https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+		);
+		this.setState({ movies, isLoading: false });
+	};
 
-function renderFood(dish){
-  return <Food name={dish.name} picture={dish.picture} />
-}
+	componentDidMount() {
+		this.getMovies();
+	}
 
-function Food(props) {
-  return (
-    <div>
-     <h1>I like {props.name}</h1>
-     <img src={props.picture} alt={props.name}/>
-    </div>
-  );
-}
-function App() {
-  return (
-    <div>
-
-      {/* <Food favorite="kimchi" /> */}
-      {foodILike.map( dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} />
-      ))}
-
-      {/* {foodILike.map(renderFood)} */}
-      
-    </div>    
-  ); 
+	render() {
+		const { isLoading, movies } = this.state;
+		return (
+			<div>
+				{isLoading
+					? "Loading..."
+					: movies.map(movie => {
+							console.log(movie);
+							return (
+								<Movie
+									key={movie.id}
+									id={movie.id}
+									year={movie.year}
+									title={movie.title}
+									summary={movie.summary}
+									poster={movie.medium_cover_image}
+								/>
+							);
+					  })}
+			</div>
+		);
+	}
 }
 
 export default App;
